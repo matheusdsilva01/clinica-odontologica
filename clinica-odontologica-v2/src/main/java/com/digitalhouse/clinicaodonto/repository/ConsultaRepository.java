f291ebe66cfa82b54f93e9666c9b1fa409da78ad
Matheus da Silva <ms25022003@gmail.com>
2021-12-14 18:34:39 -0300

    organizando o projeto em branch

--- clinica-odontologica-v2/src/main/java/com/digitalhouse/clinicaodonto/repository/ConsultaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.digitalhouse.clinicaodonto.repository;
-
-import com.digitalhouse.clinicaodonto.model.Consulta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConsultaRepository extends JpaRepository<Consulta, Integer> {
-}
